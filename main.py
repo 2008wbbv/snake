48b4f52ece1caa75b9bc027553f5970e564c597a
2008wbbv <126634454+2008wbbv@users.noreply.github.com>
2024-01-04 23:54:01 +0000

    sdff

--- main.py
+++ main.py
@@ -185,6 +185,21 @@ pygame.time.set_timer(SCREEN_UPDATE,150)
 
 main_game = MAIN()
 
+def draw_score(self):
+  score_text = str(len(self.snake.body) - 3)
+  score_surface = game_font.render(score_text, True, (56, 74, 12))
+  score_x = int(cell_size * cell_number - 60)
+  score_y = int(cell_size * cell_number - 40)
+  score_rect = score_surface.get_rect(center=(score_x, score_y))
+  apple_rect = apple.get_rect(midright=(score_rect.left, score_rect.centery))
+  bg_rect = pygame.Rect(apple_rect.left, apple_rect.top, apple_rect.width + score_rect.width + 6, apple_rect.height)
+
+  pygame.draw.rect(screen, (167, 209, 61), bg_rect)
+  screen.blit(score_surface, score_rect)
+  screen.blit(apple, apple_rect)
+  pygame.draw.rect(screen, (56, 74, 12), bg_rect, 2)
+
+
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -206,7 +221,10 @@ while True:
         if main_game.snake.direction.x != 1:
           main_game.snake.direction = Vector2(-1,0)
 
+
+  
   screen.fill((175,215,70))
   main_game.draw_elements()
   pygame.display.update()
   clock.tick(60)
+
